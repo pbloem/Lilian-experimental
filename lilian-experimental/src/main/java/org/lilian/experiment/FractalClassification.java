f8877ae4356c37aa589876f548b87ace6ae0022f
Peter Bloem <p@peterbloem.nl>
2012-04-18 17:59:17 +0200

    IFSClassifier

---
+++ lilian-experimental/src/main/java/org/lilian/experiment/FractalClassification.java
@@ -0,0 +1,25 @@
+package org.lilian.experiment;
+
+public class FractalClassification extends AbstractExperiment
+{
+
+	public FractalClassification()
+	{
+		
+	}
+	
+	@Override
+	protected void setup()
+	{
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	protected void body()
+	{
+		// TODO Auto-generated method stub
+
+	}
+
+}
