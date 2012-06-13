43cade73538eabd769aa60ff9a05067469e2d148
Peter Bloem <p@peterbloem.nl>
2012-06-13 13:27:45 +0200

    Fixed bug in multiple graph output

--- lilian-experimental/src/main/java/org/lilian/experiment/GraphMeasures.java
+++ lilian-experimental/src/main/java/org/lilian/experiment/GraphMeasures.java
@@ -2,17 +2,20 @@ package org.lilian.experiment;
 
 import static java.util.Collections.reverseOrder;
 
+import java.io.File;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 
+import org.data2semantics.tools.graphs.Edge;
+import org.data2semantics.tools.graphs.Node;
 import org.lilian.util.Series;
 import org.lilian.util.graphs.jung.Graphs;
 
 import edu.uci.ics.jung.graph.Graph;
 
 /**
- * Calculates and outputs several straigtforward graoh measures on some input 
+ * Calculates and outputs several straightforward graph measures on some input 
  * graph
  *
  * @author Peter
@@ -22,8 +25,16 @@ public class GraphMeasures<V, E> extends AbstractExperiment
 {
 	private Graph<V, E> graph;
 	
+	public @State List<Pair> pairs;  
 	public @State List<Integer> degrees;  
 	
+	
+	@Factory
+	public static GraphMeasures<Node<String>, Edge<String>> fromFile(
+			@Parameter(name="data") File file)
+	{
+		return new GraphMeasures<Node<String>, Edge<String>>(org.data2semantics.tools.graphs.Graphs.graphFromRDF(file));
+	}
 	/**
 	 * 
 	 */
@@ -56,7 +67,9 @@ public class GraphMeasures<V, E> extends AbstractExperiment
 	@Override
 	protected void setup()
 	{		
+		pairs = new ArrayList<Pair>(graph.getVertexCount());	
 		degrees = new ArrayList<Integer>(graph.getVertexCount());
+			
 	}
 
 	@Override
@@ -64,8 +77,15 @@ public class GraphMeasures<V, E> extends AbstractExperiment
 	{
 		// * Collect degrees 
 		for(V node : graph.getVertices())
+		{
 			degrees.add(graph.degree(node));
+			pairs.add(
+					new Pair(graph.degree(node), node.toString()));
+		}
+		
 		Collections.sort(degrees, reverseOrder());
+		Collections.sort(pairs, reverseOrder());
+
 	}
 
 	@Result(name="Number of nodes (vertices)")
@@ -85,5 +105,33 @@ public class GraphMeasures<V, E> extends AbstractExperiment
 	{
 		return Collections.unmodifiableList(degrees);		
 	}
+	
+	@Result(name="Degrees with labels")
+	public List<Pair> degreesWitLabels()
+	{
+		return Collections.unmodifiableList(pairs).subList(0, 100);		
+	}
+	
+	private class Pair implements Comparable<Pair>
+	{
+		int degree;
+		String node;
 		
+		public Pair(int degree, String node)
+		{
+			this.degree = degree;
+			this.node = node;
+		}
+
+		@Override
+		public int compareTo(Pair that)
+		{
+			return Double.compare(this.degree, that.degree);
+		}
+		
+		public String toString()
+		{
+			return degree + " " + node;
+		}
+	}
 }
