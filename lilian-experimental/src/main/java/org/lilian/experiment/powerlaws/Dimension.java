4e0d54f578525923d01446a84da16b6d5aa4f395
Peter Bloem <p@peterbloem.nl>
2012-08-03 18:16:42 +0200

    ...

--- lilian-experimental/src/main/java/org/lilian/experiment/powerlaws/Dimension.java
+++ lilian-experimental/src/main/java/org/lilian/experiment/powerlaws/Dimension.java
@@ -24,21 +24,26 @@ public class Dimension extends AbstractExperiment
 {
 	private List<Point> data;
 	private int samples;
+	private double epsilon;
 	
 	public @State Takens.Fit fit;
 	public @State Takens takens;
 	
 	public @State List<Double> distances;
 	public @State List<Double> generated;
+	public @State double significance;
 
 	public Dimension(
 			@Parameter(name="data") 
 				List<Point> data,
 			@Parameter(name="samples", description="The number of times to sample for maxDistance values (use -1 to use all values)")
-				int samples)
+				int samples,
+			@Parameter(name="epsilon")
+				double epsilon)
 	{
 		this.data = data;
 		this.samples = samples;
+		this.epsilon = epsilon;
 	}
 
 	@Override
@@ -55,6 +60,8 @@ public class Dimension extends AbstractExperiment
 						fit.fit() :
 						fit.fitSampled(samples);
 						
+		Global.log().info("Finished fitting model.");				
+						
 		distances = new ArrayList<Double>(samples);
 		Distance<List<Double>> metric = new EuclideanDistance();
 		for(int i : series(samples))
@@ -69,6 +76,10 @@ public class Dimension extends AbstractExperiment
 		
 		generated = takens.generate(samples);
 		Collections.sort(generated);
+		
+		Global.log().info("Calculating significance.");				
+		
+		significance = takens.significance(Takens.distances(data, metric), epsilon, samples);
 	}
 
 	@Result(name="dimension")
@@ -87,6 +98,12 @@ public class Dimension extends AbstractExperiment
 	public List<Double> distances()
 	{
 		return distances;
+	}
+	
+	@Result(name="significance")
+	public double significance()
+	{
+		return significance;
 	}
 	
 	@Result(name="generated distances", description="Distances generated form the induced distribution.")
