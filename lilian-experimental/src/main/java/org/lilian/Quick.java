1b99efea24303d0f43d10eb8c1b7de7b54b02fb6
Peter Bloem <p@peterbloem.nl>
2012-11-02 18:12:22 +0100

    ...

--- lilian-experimental/src/main/java/org/lilian/Quick.java
+++ lilian-experimental/src/main/java/org/lilian/Quick.java
@@ -1,5 +1,7 @@
 package org.lilian;
 
+import static org.lilian.util.Series.series;
+
 import java.util.List;
 import java.awt.image.BufferedImage;
 import java.io.File;
@@ -8,10 +10,13 @@ import java.io.IOException;
 import javax.imageio.ImageIO;
 
 
+import org.lilian.data.real.Cameras;
+import org.lilian.data.real.Datasets;
 import org.lilian.data.real.Draw;
 import org.lilian.data.real.Generator;
 import org.lilian.data.real.Generators;
 import org.lilian.data.real.Map;
+import org.lilian.data.real.MappedList;
 import org.lilian.data.real.Maps;
 import org.lilian.data.real.PCA;
 import org.lilian.data.real.PCAIterative;
@@ -19,6 +24,7 @@ import org.lilian.data.real.Point;
 import org.lilian.data.real.classification.Classification;
 import org.lilian.data.real.classification.Classified;
 import org.lilian.experiment.Resources;
+import org.lilian.util.Series;
 
 public class Quick
 {
@@ -42,12 +48,20 @@ public class Quick
 //		Classification.write(result, new File("/Users/Peter/Documents/datasets/classification/digits/digits.20.csv"));
 //				
 		
-		List<Point> small = Generators.rossler().generate(100000);
-		Map map = Maps.centered(small);
+		List<Point> small = Generators.rossler().generate(1000000);
+
+		Map center = Maps.centered(small),
+		    camera = Cameras.basic();
+		Map map = center.compose(camera);
 		
-		Generator<Point> gen = Generators.mapped(Generators.rossler(), map); 
+//		for(Point p  : new MappedList(small, center))
+//			System.out.println(p);
+		//map = Cameras.basic();
 		
-		BufferedImage image = Draw.draw(gen, 100000000, 1000, false);
+		Generator<Point> gen = Generators.mapped(Generators.rossler(), map);
+
+		
+		BufferedImage image = Draw.draw(gen, 100000000, 1000, true);
 		ImageIO.write(image, "PNG", new File("/Users/Peter/Desktop/quicktest.png"));
 	}
 
