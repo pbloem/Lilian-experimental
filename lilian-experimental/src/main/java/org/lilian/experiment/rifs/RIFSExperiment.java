d5cf3f756ec3cf17fbcf4cce355016e85fe30d46
Peter Bloem <p@peterbloem.nl>
2013-02-01 20:29:41 +0100

    ...

--- lilian-experimental/src/main/java/org/lilian/experiment/rifs/RIFSExperiment.java
+++ lilian-experimental/src/main/java/org/lilian/experiment/rifs/RIFSExperiment.java
@@ -11,6 +11,7 @@ import javax.imageio.ImageIO;
 import org.lilian.Global;
 import org.lilian.data.real.Point;
 import org.lilian.data.real.Similitude;
+import org.lilian.data.real.fractal.random.ChoiceTree;
 import org.lilian.data.real.fractal.random.DiscreteRIFS;
 import org.lilian.data.real.fractal.random.RIFSEM;
 import org.lilian.data.real.fractal.random.RIFSs;
@@ -20,17 +21,26 @@ import org.lilian.util.Series;
 
 public class RIFSExperiment extends AbstractExperiment
 {
-	private static int M = 50;
-	private static int N = 100000;
-	private static int DEPTH = 8;
+	// * stable: 100 datasets, 128 samples per dataset, depth 6 (on koch and cantor)
+	
+	// * If we start with the correct trees for the data, the cantor model is
+	//   successfully induced from a random initial model. (in less than 
+	//   five generations)
+	
+	private static int M = 100;
+	private static int N = 10000;
+	private static int N_SAMPLE = 128;
+	private static int DEPTH = 6;
 	private static int RES = 200;
 	private static int NUM_RANDOM = 3;
 	
 	private int generations = 100;
 	
-	private static DiscreteRIFS<Similitude> TARGET = RIFSs.koch2UpDown(); 
+	private static DiscreteRIFS<Similitude> TARGET = RIFSs.cantor(); 
 	
-	private List<List<Point>> data = new ArrayList<List<Point>>(N);
+	private List<List<Point>> data = new ArrayList<List<Point>>(M);
+	private List<ChoiceTree> dataTrees = new ArrayList<ChoiceTree>(M);
+	
 	private File genDir;
 	
 	@State
@@ -40,7 +50,11 @@ public class RIFSExperiment extends AbstractExperiment
 	{
 		
 		for(int i : Series.series(M))
-			data.add(TARGET.randomInstance(N, DEPTH));
+		{
+			ChoiceTree tree = ChoiceTree.random(TARGET, DEPTH);
+			dataTrees.add(tree);
+			data.add(TARGET.generator(tree).generate(N));
+		}
 	}
 	
 	@Override
@@ -48,7 +62,8 @@ public class RIFSExperiment extends AbstractExperiment
 	{
 		// DiscreteRIFS<Similitude> initial = RIFSs.initialSphere(2, 2, 2, 1.0, 0.33);
 		DiscreteRIFS<Similitude> initial = TARGET;
-		em = new RIFSEM(initial, data, DEPTH, 1024, 0.0001, 0.3);
+	
+		em = new RIFSEM(initial, data, DEPTH, N_SAMPLE, 0.00001, 0.3);
 		
 		genDir = new File(dir, "generations/");
 		genDir.mkdirs();
