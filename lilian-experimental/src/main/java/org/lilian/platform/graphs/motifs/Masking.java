08ae5013fc80f6bab4a3ae8843be6d77a715ef2e
Peter Bloem <p@peterbloem.nl>
2014-07-05 01:07:13 +0200

    ...

--- lilian-experimental/src/main/java/org/lilian/platform/graphs/motifs/Masking.java
+++ lilian-experimental/src/main/java/org/lilian/platform/graphs/motifs/Masking.java
@@ -1,6 +1,7 @@
 package org.lilian.platform.graphs.motifs;
 
 import static java.util.Collections.reverseOrder;
+import static org.data2semantics.platform.util.Series.series;
 
 import java.io.File;
 import java.io.FileWriter;
@@ -24,6 +25,7 @@ import org.nodes.DGraph;
 import org.nodes.DLink;
 import org.nodes.DNode;
 import org.nodes.DTGraph;
+import org.nodes.DTLink;
 import org.nodes.DTNode;
 import org.nodes.DegreeComparator;
 import org.nodes.Graph;
@@ -38,6 +40,7 @@ import org.nodes.algorithms.Nauty;
 import org.nodes.compression.EdgeListCompressor;
 import org.nodes.compression.MotifCompressor;
 import org.nodes.compression.MotifVar;
+import org.nodes.compression.MotifVarTags;
 import org.nodes.compression.NeighborListCompressor;
 import org.nodes.data.Data;
 import org.nodes.data.GML;
@@ -54,12 +57,12 @@ import org.nodes.util.Order;
 
 import au.com.bytecode.opencsv.CSVWriter;
 
-@Module(name = "Masking Motif extraction", description = "Tests different methods of Motif extraction to avoid overlap")
+@Module(name = "Masking Motif extraction")
 public class Masking
 {	
 	private static final int MIN_OCCURRENCES = 10;
 
-	private DGraph<String> data;
+	private DTGraph<String, String> data;
 	private DGraph<String> top, second, third;
 	private int samples;
 	private int minSize, maxSize;
@@ -79,7 +82,7 @@ public class Masking
 	private Generator<Integer> intGen;
 
 	public Masking(
-			@In(name = "data", print = false) DGraph<String> data,
+			@In(name = "data", print = false) DTGraph<String, String> data,
 			@In(name = "samples") int samples,
 			@In(name = "min size") int minSize,
 			@In(name = "max size") int maxSize,
@@ -109,27 +112,12 @@ public class Masking
 		
 		// * Places in the graph where each motif occurs
 		Map<DGraph<String>, List<List<Integer>>> occurrences = new LinkedHashMap<DGraph<String>, List<List<Integer>>>();
-		// * Those nodes that have been taken by one of the occurrence. If a new occurrence contains one of these,
+		// * Those nodes that have been taken by one of the occurrences. If a new occurrence contains one of these,
 		//   it will not be added
 		Map<DGraph<String>, Set<Integer>> taken = new LinkedHashMap<DGraph<String>, Set<Integer>>();
-		
-		// * Hub removal
-		List<DNode<String>> hubs = new ArrayList<DNode<String>>(remHubs);
-		if (remHubs > 0)
-		{
-			List<DNode<String>> nodes = new ArrayList<DNode<String>>(
-					data.nodes());
-			Collections.sort(nodes,
-					reverseOrder(new DegreeComparator<String>()));
-
-			hubs.addAll(nodes.subList(0, remHubs));
-		}
-
-		for (DNode<String> node : hubs)
-			System.out.println("HUB " + node + "\t" + node.degree());
 
 		SubgraphGenerator<String> gen = new SubgraphGenerator<String>(data,
-				intGen, hubs);
+				intGen, Collections.EMPTY_LIST);
 
 		Functions.tic();
 
@@ -141,36 +129,37 @@ public class Masking
 			
 			// * Sample a subgraph
 			SubgraphGenerator<String>.Result result = gen.generate();
-			DGraph<String> sub = Subgraph.dSubgraphIndices(data,
+			DTGraph<String, String> subLabeled = Subgraph.dtSubgraphIndices(data,
 					result.indices());
-			sub = Graphs.blank(sub, "");
-
-			// * Reorder nodes to canonical ordering
-			Order canonical = Nauty.order(sub, comp);
-			sub = Graphs.reorder(sub, canonical);
-			List<Integer> indices = canonical.apply(result.indices());
+			DGraph<String> subBlanked = Graphs.blank(subLabeled, "");
 			
 			// * Check if any of the nodes of the occurrence have been used already
 			boolean overlaps;
-			if(! taken.containsKey(sub))
+			if(! taken.containsKey(subBlanked))
 				overlaps = false;
 			else
-				overlaps = Functions.overlap(taken.get(sub), indices) > 0;
+				overlaps = Functions.overlap(taken.get(subBlanked), result.indices()) > 0;
 			
 			// * Add it as an occurrence	
 			if(! overlaps)
 			{	
-				fm.add(sub, correct ? result.invProbability() : 1.0);
-	
-				if (! occurrences.containsKey(sub))
-					occurrences.put(sub, new ArrayList<List<Integer>>());
-	
-				occurrences.get(sub).add(indices);
+				// * Reorder nodes to canonical ordering
+				Order canonical = Nauty.order(subBlanked, comp);
+				subBlanked = Graphs.reorder(subBlanked, canonical);
 				
-				if (! taken.containsKey(sub))
-					taken.put(sub, new HashSet<Integer>());
+				List<Integer> indices = canonical.apply(result.indices());
 				
-				taken.get(sub).addAll(indices);
+				fm.add(subBlanked, correct ? result.invProbability() : 1.0);
+	
+				if (! occurrences.containsKey(subBlanked))
+					occurrences.put(subBlanked, new ArrayList<List<Integer>>());
+	
+				occurrences.get(subBlanked).add(indices);
+				
+				if (! taken.containsKey(subBlanked))
+					taken.put(subBlanked, new HashSet<Integer>());
+				
+				taken.get(subBlanked).addAll(indices);
 			}
 		}
 
@@ -192,10 +181,11 @@ public class Masking
 		CSVWriter writer = new CSVWriter(new FileWriter(
 				new File(Global.getWorkingDir(), "motifs.csv")));
 		
+		int i  = 0;
 		for (DGraph<String> sub : tokens)
 		{
 			System.out.println("Starting motif (" + fm.frequency(sub) + ")" + sub);
-			int n = sub.size();
+			int nMask = sub.size() + sub.numLinks();
 			
 			if(fm.frequency(sub) < MIN_OCCURRENCES)
 				break;
@@ -210,7 +200,7 @@ public class Masking
 			List<List<Integer>> currentTopOccurrences = null;
 			List<List<String>> currentTopLabels = null;
 			
-			for (BitString mask : BitString .all(n))
+			for(BitString mask : BitString.all(nMask))
 			{				
 				System.out.print(".");
 				
@@ -219,7 +209,7 @@ public class Masking
 
 				motif = mask(sub, mask, data,
 						occurrences.get(sub), occOut, labels);
-
+				
 				MotifVar mv = new MotifVar(data, motif,occOut);
 				bits = mv.size();
 				
@@ -235,7 +225,30 @@ public class Masking
 			List<Integer> occChoice = Functions.choose(currentTopOccurrences);
 			DGraph<String> randomOcc = Subgraph.dSubgraphIndices(data, occChoice);
 			
-			writer.writeNext(new String[]{currentTopOccurrences.size()+"", currentTopBits+"", currentTopMotif+"", randomOcc+""});
+			writer.writeNext(new String[]{i+"", currentTopOccurrences.size()+"", currentTopBits+"", currentTopMotif+"", randomOcc+""});
+			
+			// * write the occurrences to a CSV file
+			CSVWriter motifWriter = new CSVWriter(new FileWriter(
+					new File(Global.getWorkingDir(), "occurrences.motif"+i+".csv")));
+			
+			String[] motifLabels = new String[currentTopMotif.size()];
+			for(int k : series(currentTopMotif.size()))
+				motifLabels[k] = currentTopMotif.get(k).label();
+			
+			motifWriter.writeNext(motifLabels);
+			
+			for(List<Integer> occurrenceIndices : currentTopOccurrences)
+			{
+				DGraph<String> occurrence = Subgraph.dSubgraphIndices(data, occurrenceIndices);
+				String[] occLabels = new String[occurrence.size()];
+				for(int k : series(occurrence.size()))
+					occLabels[k] = occurrence.get(k).label();
+				
+				motifWriter.writeNext(occLabels);
+			}
+			
+			motifWriter.close();
+			
 			System.out.println();
 			
 			if(currentTopBits < topBits)
@@ -246,7 +259,9 @@ public class Masking
 				topLabels = currentTopLabels;
 			}	
 			
+			i++;
 		}
+		writer.close();
 		
 		System.out.println("top motif " + topMotif);
 		
@@ -326,11 +341,82 @@ public class Masking
 
 		return (int) fm.frequency(fm.maxToken());
 	}
+	
+	/**
+	 * Returns the labels of the given occurrence in canonical order
+	 * 
+	 * @param graph
+	 * @param indices
+	 * @return
+	 */
+	private static List<String> labels(DTGraph<String, String> graph, List<Integer> indices)
+	{
+		List<String> labels = new ArrayList<String>();
+		
+		DTGraph<String, String> sub = Subgraph.dtSubgraphIndices(graph, indices);
+				
+		for(Node<String> node : sub.nodes())
+			labels.add(node.label());
+		
+		// * We create our own loop through the links, to make sure that it doesn't 
+		//   depend on the graph implementation.
+		for(DTNode<String, String> node : sub.nodes())
+		{
+			int from = node.index();
+			
+			Set<Integer> tos = new HashSet<Integer>();
+			for(DTLink<String, String> link : node.linksOut())
+				tos.add(link.to().index());
+			
+			List<Integer> listTos = new ArrayList<Integer>(tos);
+			Collections.sort(listTos);
+			
+			for(int to : listTos)
+			{
+				List<String> tags = new ArrayList<String>();
+				for(DTLink<String, String> link : sub.get(from).linksOut(sub.get(to)))
+					tags.add(link.tag());
+				
+				Collections.sort(tags);
+				
+				labels.addAll(tags);
+			}	
+		}
+		
+		return labels;
+	}
+	
+	private static DTGraph<String, String> motif(DGraph<String> subgraph, List<String> labels)
+	{	
+		DTGraph<String, String> copy = new MapDTGraph<String, String>();
+		int i = 0;
+		
+		for(Node<String> node : subgraph.nodes())
+			copy.add(labels.get(i++));
+	
+		for(DNode<String> node : subgraph.nodes())
+		{
+			int from = node.index();
+			Set<Integer> tos = new HashSet<Integer>();
+			for(DLink<String> link : node.linksOut())
+				tos.add(link.to().index());
+			
+			List<Integer> listTos = new ArrayList<Integer>(tos);
+			Collections.sort(listTos);
+			
+			for(int to : listTos)
+				for(DLink<String> link : subgraph.get(from).linksOut(subgraph.get(to)))
+					copy.get(from).connect(copy.get(to), labels.get(i++));
+		}
+		
+		return copy;
+	}
+
 
 	/**
 	 * Applies mask to substring.
 	 * 
-	 * For the nonmasked nodes, it chooses the labels from the occurrences that
+	 * For the nonmasked nodes and links, it chooses the labels from the occurrences that
 	 * maximize the number of occurrences for the masked motif.
 	 * 
 	 * @param sub
@@ -340,58 +426,43 @@ public class Masking
 	 * @param occurrencesOut
 	 * @return
 	 */
-	public static DGraph<String> mask(DGraph<String> sub, BitString mask,
-			DGraph<String> data, List<List<Integer>> occurrences,
+	public static DTGraph<String, String> mask(DGraph<String> sub, BitString mask,
+			DTGraph<String, String> data, List<List<Integer>> occurrences,
 			List<List<Integer>> occurrencesOut, List<List<String>> labels)
 	{
-		// * Tally the frequencies of non-masked nodes
-		int unmasked = mask.numZeros();
 		
-		// - Indices of unmasked bits
-		List<Integer> indices = new ArrayList<Integer>(unmasked);
-		for (int i : Series.series(mask.size()))
-			if (!mask.get(i))
-				indices.add(i);
-
-		List<String> choice;
 		FrequencyModel<List<String>> fm = new FrequencyModel<List<String>>();
-		
 		for (List<Integer> occurrence : occurrences)
 		{
-			choice = new ArrayList<String>(unmasked);
-			for (int index : indices)
-				choice.add(data.get(occurrence.get(index)).label());
-	
-			fm.add(choice);
+			List<String> sequence = labels(data, occurrence);
+			
+			assert(sequence.size() == mask.size());
+			for(int i : series(mask.size()))
+				if(mask.get(i))
+					sequence.set(i, MotifVar.VARIABLE_SYMBOL);
+			
+			fm.add(sequence);
 		}
 
-		choice = fm.maxToken();
-
+		List<String> choice = fm.maxToken();
+		
 		// * Filter out the non-matching occurrences
 		for (List<Integer> occurrence : occurrences)
-			if (matches(choice, indices, occurrence, data))
-				occurrencesOut.add(occurrence);
+		{
+			List<String> sequence = labels(data, occurrence);
+			
+			for(int i : series(sequence.size()))
+			
+			if(! (choice.get(i).equals(MotifVar.VARIABLE_SYMBOL) || choice.get(i).equals(sequence.get(i))))
+				continue;
+			
+			occurrencesOut.add(occurrence);
+		}
 		
 		System.out.print(occurrencesOut.size());
 
 		// * Copy the subgraph and mask out the 1s
-		DGraph<String> copy = new MapDTGraph<String, Object>();
-		
-		Iterator<String> it = choice.iterator();
-		for (int j : Series.series(sub.size()))
-			if (mask.get(j))
-				copy.add(MotifVar.VARIABLE_SYMBOL);
-			else
-				copy.add(it.next());
-
-		for (DLink<String> link : sub.links())
-		{
-			int from = link.from().index(), to = link.to().index();
-
-			copy.get(from).connect(copy.get(to));
-		}
-
-		return copy;
+		return motif(sub, choice);
 	}
 
 	/**
@@ -419,21 +490,5 @@ public class Masking
 		return means;
 	}
 
-	private static boolean matches(List<String> choice, List<Integer> indices,
-			List<Integer> occurrence, DGraph<String> data)
-	{
-		for (int i : Series.series(choice.size()))
-		{
-			int indexOcc = indices.get(i);
-			int indexGraph = occurrence.get(indexOcc);
 
-			String graphLabel = data.get(indexGraph).label();
-			String choiceLabel = choice.get(i);
-
-			if (!choiceLabel.equals(graphLabel))
-				return false;
-		}
-
-		return true;
-	}
 }
